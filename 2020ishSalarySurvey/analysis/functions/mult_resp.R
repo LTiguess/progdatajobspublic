2b3fe6217b5d157d2049f89d7d1fdbfb71ea5281
Mahelet Fekade <maheletfekade@mahelets-air.myfiosgateway.com>
2021-02-22 17:21:13 -0500

    add functions

---
+++ 2020ishSalarySurvey/analysis/functions/mult_resp.R
@@ -0,0 +1,87 @@
+# for multiple response questions 
+# helpful resource: https://stackoverflow.com/questions/9265003/analysis-of-multiple-response
+# to do, make it easy to clean-up names + make it easy to order the variables
+
+mult_resp <- function(df, question.prefix, raw_df = F,
+                      order_prop = F, last_ord = NULL) {
+  # x = data frame 
+  # question.prefix = charater string
+  # raw_df = if T, return markdown, if F return HTML
+  # order_prop = if T, order by proprotion, if F, order alphabetical
+  # last_ord = whatever you order by, the inputs here are put in the bottom of the ordering
+  
+  
+  
+  # get the number of cases (total people who took the survey)
+  n_cases <- nrow(df) 
+  
+  
+  # create a df of people who answered this question 
+  df2 <- 
+    # select the columns we are interested in analysing 
+    select(df, contains(question.prefix)) %>%
+    mutate_all(funs( as.logical(.)))  %>%
+    mutate(
+      # for each respondent, count the number of 'True' values 
+      n_qs_answered = rowSums(., na.rm = TRUE))  %>% 
+    # filter anyone who didn't anwser the question 
+    filter(n_qs_answered > 0)
+  
+  # get count of people who anwsered the question 
+  n_res <- nrow(df2)
+  
+  # for each response, get total number of responses 
+  freq <- df2 %>% select(-n_qs_answered) %>%
+    gather("response", "answer_q") %>%
+    group_by(response) %>%
+    summarize(
+      unweighted_prop  = sum(answer_q, na.rm = T)/ n_res)
+  
+  
+  
+  # Order if applicable
+  if(order_prop == TRUE){
+    freq <- freq %>%
+      arrange(desc(unweighted_prop))
+    var_char <- as.character(freq$response)
+    freq$response <- factor(
+      freq$response,
+      levels = c(var_char[! var_char %in% last_ord], last_ord))
+    freq <- freq %>%
+      arrange(response)
+  }
+  
+  freq <- freq %>%
+    select(response, unweighted_prop) %>%
+    mutate(unweighted_prop = formattable::percent(unweighted_prop, 0)
+           #   unweighted_n = formattable::digits(unweighted_n, 0)
+    )
+  
+  
+  if(raw_df == TRUE){
+    freq
+  } else {
+    freq %>%
+      rename(' ' = response,
+             'Proportion' = unweighted_prop) %>%
+      formattable::formattable(list(
+        Proportion = formattable::proportion_bar()),
+        align = c('l', 'r')
+      )
+  }
+
+  
+  # # Returns the HTML stuff
+  # .pretty_tt <- function(freq){
+  #   
+  #   freq %>%
+  #     rename(' ' = response,
+  #            'Proportion' = unweighted_prop) %>%
+  #     formattable::formattable(list(
+  #       Proportion = formattable::proportion_bar()),
+  #       align = c('l', 'r')
+  #     )
+  # }
+  
+}
+
---
+++ 2020ishSalarySurvey/analysis/functions/tt_function.R
@@ -0,0 +1,99 @@
+# Functions - Clean Table with Counts + Proportions 
+tt <- function(x, weights = NULL,
+               lvls = NULL, recode_na = 'Missing',
+               raw_df = F,
+               order_prop = F, last_ord = NULL){
+  
+  # x = vector of values
+  # weights = weights
+  # lvls = level order in x (if you want it ordered)
+  # necode_na = how to recode NA values, defaults to 'Refused'
+  # raw_df = if T, return markdown, if F return HTML
+  # order_prop = if T, order by proprotion, if F, order alphabetical
+  # last_ord = whatever you order by, the inputs here are put in the bottom of the ordering
+  
+  
+  
+  # conditions to run
+  stopifnot(#is.vector(x),
+    length(x) > 0
+  )
+  
+  # if weights is null, set to 1
+  if(is.null(weights)) weights <- rep(1, length(x))
+  
+  if(is.factor(x) & sum(is.na(x)) > 0){
+    x <- factor(x, levels = c(levels(x), recode_na))
+  }
+  
+  if(is.character(x) & sum(is.na(x)) > 0){
+    x <- factor(x, levels = c(unique(x), recode_na))
+  }
+  
+  # add ordered factor
+  if(!is.null(lvls)){
+    if(sum(is.na(x)) > 0) lvls <- c(lvls, recode_na)
+    x <- factor(x, levels = lvls, ordered = T)
+  } 
+  
+  # add NA values
+  if(!is.null(recode_na) & sum(is.na(x)) > 0) x[is.na(x)] <- recode_na
+  
+  # create crosstab
+  df <- data.frame(variable = x) %>%
+    # group by variable
+    group_by(variable) %>%
+    # get raw counts
+    summarise(unweighted_n = n()) %>%
+    ungroup() %>%
+    tidyr::complete(variable, fill = list(unweighted_n = 0)) %>%
+    # create probabilities
+    mutate(unweighted_prop = unweighted_n/sum(unweighted_n))
+  
+  # Order if applicable
+  if(order_prop == TRUE){
+    df <- df %>%
+      arrange(desc(unweighted_prop))
+    var_char <- as.character(df$variable)
+    df$variable <- factor(
+      df$variable,
+      levels = c(var_char[! var_char %in% last_ord], last_ord))
+    df <- df %>%
+      arrange(variable)
+  }
+  
+  df <- df %>%
+    select(variable, unweighted_prop, unweighted_n) %>%
+    mutate(unweighted_prop = formattable::percent(unweighted_prop, 0),
+           unweighted_n = formattable::digits(unweighted_n, 0))
+  
+  
+  if(raw_df == TRUE){
+    df
+  } else {
+ #   Returns the HTML stuff
+    df %>%
+      rename(' ' = variable,
+             'Proportion' = unweighted_prop,
+             'N' = unweighted_n) %>%
+      formattable::formattable(list(
+        Proportion = formattable::proportion_bar()),
+        align = c('l', 'r', 'r'))
+  }
+  
+
+# 
+# # Returns the HTML stuff
+# .pretty_tt <- function(df){
+#   
+#   df %>%
+#     rename(' ' = variable,
+#            'Proportion' = unweighted_prop,
+#            'N' = unweighted_n) %>%
+#     formattable::formattable(list(
+#       Proportion = formattable::proportion_bar()),
+#       align = c('l', 'r', 'r'))
+#   
+#   
+# }
+}
