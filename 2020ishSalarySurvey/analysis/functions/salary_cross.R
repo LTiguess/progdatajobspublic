5eedc3760fe5dedf029ba2c28ab9966f219e604b
mfekade <16806809+mahelet-fekade@users.noreply.github.com>
2021-04-05 23:02:35 -0400

    more edits

--- 2020ishSalarySurvey/analysis/functions/salary_cross.R
+++ 2020ishSalarySurvey/analysis/functions/salary_cross.R
@@ -1,7 +1,9 @@
 # Function: Produces crosstable of salary with a 
-salary_cross <- function(x, variable, salary,
+salary_cross <- function(x, 
+                         variable,
+                         salary,
                          salary_bin, 
-                      #   salary_bin_numeric,
+                         salary_bin_numeric,
                          order_by, # N, levels, alpha
                          lvls = NULL,
                          last_ord = NULL
@@ -20,21 +22,32 @@ salary_cross <- function(x, variable, salary,
   
 # clean df   
 df <- data.frame(x) %>%
-  select({{variable}}, {{salary}}, {{salary_bin}}) %>% 
+  select({{variable}},
+         {{salary}},
+         {{salary_bin}}, 
+         {{salary_bin_numeric}}
+         ) %>% 
+  rename(variable = {{variable}}, 
+         salary = {{salary}}, 
+         salary_bin = {{salary_bin}},
+         salary_bin_numeric = {{salary_bin_numeric}}
+         )  %>% 
   # remove anyone that is missing the categorical variable and/or salary data 
-  filter(!is.na({{variable}}), !is.na({{salary}}), !is.na({{salary_bin}})) %>% 
-  mutate(salary_bin_numeric = as.numeric({{salary_bin}}))
+  filter(!is.na(variable),
+         !is.na(salary),
+         !is.na(salary_bin), 
+         !is.na( salary_bin_numeric))
 
 # crosswalk to go from numeric salary bin to salary bin 
 
-salary_bin_crosswalk <- df %>% distinct({{salary_bin}}, salary_bin_numeric)
+salary_bin_crosswalk <- df %>% distinct(salary_bin, salary_bin_numeric)
   
 
 # create crosstab 
 cross_table <- df  %>%
-  group_by({{variable}}) %>%
+  group_by(variable) %>%
   summarise(
-    mean = mean({{salary}}),
+    mean = mean(salary),
     median_numeric = round(median(salary_bin_numeric), 0),
     n = n()
   ) %>%
@@ -49,19 +62,17 @@ cross_table <- df  %>%
 if(order_by == "n"){
   # order by mean
   cross_table <- cross_table %>% 
-    rename(variable = {{variable}}) %>% 
     arrange(desc(n))
 } else if (order_by == "levels") {
    # order by prespecified levels
   cross_table <- cross_table %>%
     mutate(
-      variable = factor({{variable}},
+      variable = factor(variable,
              levels = c(lvls[!lvls %in% last_ord], last_ord))
       ) %>%
     arrange(variable)
 } else if (order_by == "alpha") {
     cross_table <- cross_table %>%
-      rename(variable = {{variable}}) %>%
       arrange(variable) # asc order
   } else {
     stop("argument 'order_by' value must either be 'n', 'levels', or 'alpha'")
@@ -71,7 +82,7 @@ if(order_by == "n"){
 # clean-up output 
 clean_table <- cross_table %>%
   # select only the necessary columns
-  select(variable, n, mean, median = {{salary_bin}}) %>%  
+  select(variable, n, mean, median = salary_bin) %>%  
   mutate(mean = formattable::currency(mean, digits = 0))  %>%  
   # mutate(mean = ifelse(n >= 5, formattable::currency(mean), NA),
   #        median = ifelse(n >= 5, median, NA)) %>%
