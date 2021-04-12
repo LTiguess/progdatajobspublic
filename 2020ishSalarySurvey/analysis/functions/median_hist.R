10373c7f14028dd055102373827114f9dd146604
Olivia Robinson <olivia.mae.robinson@gmail.com>
2021-04-11 20:16:18 -0400

    olivia version of analysis: main change is moving salary graphs to a function and implementing filters for outlier salaries

--- 2020ishSalarySurvey/analysis/functions/median_hist.R
+++ 2020ishSalarySurvey/analysis/functions/median_hist.R
@@ -1,4 +1,4 @@
-# Creates a historgram and plots (roundest to the nearest whole number) median
+# Creates a histogram and plots (roundest to the nearest whole number) median
 # removes missing values
 
 median_hist <- function(x, x_label, med_label, med_label_x_cord, med_label_y_cord, color_hist){
@@ -10,17 +10,19 @@ median_hist <- function(x, x_label, med_label, med_label_x_cord, med_label_y_cor
     geom_vline(xintercept = 
                  median(
                    x[!is.na(x)]),
-               color = "black") + 
+               color = "#440154FF", 
+               size = 2) + 
     annotate(geom = "text", 
              label = paste(
                med_label,
                round(median(x[!is.na(x)])) 
-               ),
-             x =  med_label_x_cord,  
-             y = med_label_y_cord
-             ) +
+             ),
+             x = med_label_x_cord,  
+             y = med_label_y_cord, 
+             color = "#440154FF"
+    ) +
     xlab(x_label) +
-    ylab("Count") +
-    theme_bw() 
+    ylab("Number of Respondents") +
+    theme_minimal() 
 }
 
---
+++ 2020ishSalarySurvey/analysis/functions/plot_salary.R
@@ -0,0 +1,33 @@
+# Function: Create Scatterplot of Salary Crossed by a Variable
+#' @param x data frame 
+#' @param variable
+#' 
+plot_salary <- function(dat, variable){
+  
+  df <- dat %>%
+    select(
+      {{variable}}, 
+      compensation, 
+      years_worked_political_data, 
+      years_worked_political, 
+      years_worked_data, 
+      years_worked_total,
+      work_hist_eligible
+    ) %>% filter(
+      work_hist_eligible == TRUE, 
+      !is.na(.data[[variable]]),
+      grepl('Prefer not to answer', .data[[variable]]) == FALSE
+    )
+  
+  ## creates chart
+  plot <- df %>% 
+    ggplot(aes(x = years_worked_total, y = compensation, color = .data[[variable]])) +
+    geom_jitter(stat = "identity", width = 2, height = 2) +
+    geom_smooth(method = "loess", se = FALSE) +
+    theme_light()+
+    theme(legend.title=element_blank())+
+    xlab("Years of Experience")+
+    ylab("Salary")
+  
+  return(plot)
+} 
